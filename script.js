8270a0d09512c74ab7703368a7b9c0c084b8ecc7
DigitalBrainStudios <140276138+DigitalBrainStudios@users.noreply.github.com>
2025-05-11 12:07:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// script.js
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+function showAlert() {
+  alert("Thank you for subscribing!");
+}
+
 <button onclick="scrollToSection('trending')">Read Blogs</button>
 <button onclick="scrollToSection('subscribe')">Subscribe Now</button>
 <button onclick="showAlert()">Click Me</button>
